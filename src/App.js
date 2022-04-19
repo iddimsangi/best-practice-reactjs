39671f6520c946f4c30b499c27e782aae0525834
iddimsangi <adammsangi@gmail.com>
2022-04-19 10:57:07 +0300

    improve: add routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import AddTodoForm from './components/AddTodoForm';
-import TodoList from './components/TodoList';
-import TotalCompleteItems from './components/TotalCompleteItems';
-
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import AddTodoForm from "./components/AddTodoForm";
+import TodoList from "./components/TodoList";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 const App = () => {
-	return (
-		<div className='container bg-white p-4 mt-5'>
-			<h1>My Todo List</h1>
-			<AddTodoForm />
-			<TodoList />
-			<TotalCompleteItems />
-		</div>
-	);
+  return (
+    <div className="container bg-white p-4 mt-5">
+      <h1>My Todo List</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AddTodoForm />} />
+          <Route path="/todoList" element={<TodoList />} />
+          {/* <Route path="/TotalCompleteItems" element={<TotalCompleteItems />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
